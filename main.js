65982f514435dde3e3f04c2e81903404ed4ebb5c
soumnayk <soumyadeep.nayak@justdial.com>
2024-06-22 18:00:20 +0530

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const { crawlPage } = require("./crawl");
+
+function main(){
+    if(process.argv.length < 3){
+        console.log("no website found");
+        process.exit(1)
+    }
+    if(process.argv.length > 3){
+        console.log("too many command line agrs");
+        process.exit(1)
+    }
+
+    const baseUrl = process.argv[2]
+
+    console.log(`STARTING CRAWL FOR ${baseUrl}`);
+
+    crawlPage(baseUrl)
+}
+
+main()
